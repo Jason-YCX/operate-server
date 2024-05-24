743dfd585d4776fd8988be77a117d864c95fb747
喻储旭 <yuchuxu@yuchuxudeMacBook-Air.local>
2024-05-24 16:57:00 +0800

    update

--- db/enter.go
+++ db/enter.go
@@ -18,7 +18,7 @@ func InitDB() {
 	}
 	time.Local = loc
 	// 数据库连接信息
-	dsn := "root:Jason123....@tcp(127.0.0.1:3306)/JASON_SERVER?parseTime=true&loc=Local"
+	dsn := "root:Jason123....@tcp(localhost:3306)/JASON_SERVER?parseTime=true&loc=Local"
 
 	// 打开数据库连接
 	myDB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
